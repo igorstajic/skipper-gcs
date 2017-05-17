6d11fb84a558c3e6e55b71bd3ef9804f7c53ebea
igor <igorstajic273@gmail.com>
2017-05-17 14:24:55 +0200

    added resize dimension param

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /**
  * Module dependencies
  */
-var debug = require('debug')('skipper-gcs');
-var qs = require('querystring');
-var Writable = require('stream').Writable;
-var _ = require('lodash');
-var gcloud = require('gcloud');
-var concat = require('concat-stream');
-var mime = require('mime');
-var gm = require('gm');
+const debug = require('debug')('skipper-gcs');
+const qs = require('querystring');
+const Writable = require('stream').Writable;
+const _ = require('lodash');
+const gcloud = require('gcloud');
+const concat = require('concat-stream');
+const mime = require('mime');
+const gm = require('gm');
 
 /**
  * skipper-gcs
@@ -22,44 +22,44 @@ module.exports = function GCSStore(globalOpts) {
     email: '',
     bucket: '',
     folder: 'photos',
-    scopes: ['https://www.googleapis.com/auth/devstorage.full_control']
+    scopes: ['https://www.googleapis.com/auth/devstorage.full_control'],
   });
-  var gcs = gcloud.storage({
+  const gcs = gcloud.storage({
     projectId: globalOpts.projectId,
-    keyFilename: globalOpts.keyFilename
+    keyFilename: globalOpts.keyFilename,
   });
-  var bucket = gcs.bucket(globalOpts.bucket);
+  const bucket = gcs.bucket(globalOpts.bucket);
 
-  var adapter = {
-    ls: function (dirname, cb) {
+  const adapter = {
+    ls(dirname, cb) {
       bucket.getFiles({
-        prefix: dirname
-      }, function (err, files) {
+        prefix: dirname,
+      }, (err, files) => {
         if (err) {
-          cb(err)
+          cb(err);
         } else {
           files = _.pluck(files, 'name');
           cb(null, files);
         }
       });
     },
-    read: function (fd, cb) {
-      var remoteReadStream = bucket.file(fd).createReadStream();
+    read(fd, cb) {
+      const remoteReadStream = bucket.file(fd).createReadStream();
       remoteReadStream
-        .on('error', function (err) {
+        .on('error', (err) => {
           cb(err);
         })
-        .on('response', function (response) {
+        .on('response', (response) => {
           // Server connected and responded with the specified status and headers.
         })
-        .on('end', function () {
+        .on('end', () => {
           // The file is fully downloaded.
         })
-        .pipe(concat(function (data) {
+        .pipe(concat((data) => {
           cb(null, data);
         }));
     },
-    rm: function (fd, cb) {
+    rm(fd, cb) {
       return cb(new Error('TODO'));
     },
     /**
@@ -71,82 +71,79 @@ module.exports = function GCSStore(globalOpts) {
     receive: function GCSReceiver(options) {
       options = options || {};
       options = _.defaults(options, globalOpts);
-      var receiver__ = Writable({
-        objectMode: true
+      const receiver__ = Writable({
+        objectMode: true,
       });
       // This `_write` method is invoked each time a new file is received
       // from the Readable stream (Upstream) which is pumping filestreams
       // into this receiver.  (filename === `__newFile.filename`).
       receiver__._write = function onFile(__newFile, encoding, done) {
-        var metadata = {};
+        const metadata = {};
         _.defaults(metadata, options.metadata);
         metadata.contentType = mime.lookup(__newFile.fd);
-        var allUploads = [];
+        const allUploads = [];
         allUploads.push(new Promise((resolve, reject) => {
-          var file = bucket.file(globalOpts.folder + '/' + __newFile.fd);
-          var stream = file.createWriteStream({
-            metadata: metadata
+          const file = bucket.file(`${globalOpts.folder}/${__newFile.fd}`);
+          const stream = file.createWriteStream({
+            metadata,
           });
-          stream.on('error', function (err) {
-            reject(err)
-            return;
-          })
-          stream.on('finish', function () {
+          stream.on('error', (err) => {
+            reject(err);
+          });
+          stream.on('finish', () => {
             __newFile.extra = file.metadata;
-            __newFile.extra.Location = 'https://storage.googleapis.com/' + globalOpts.bucket + '/' + globalOpts.folder + '/' + __newFile.fd;
+            __newFile.extra.Location = `https://storage.googleapis.com/${globalOpts.bucket}/${globalOpts.folder}/${__newFile.fd}`;
             if (globalOpts.public) file.makePublic();
             resolve();
           });
           __newFile.pipe(stream);
-        }))
+        }));
         if (globalOpts.resize) {
           allUploads.push(new Promise((resolve, reject) => {
-            var file = bucket.file('photos/resized/' + __newFile.fd);
-            var stream = file.createWriteStream({
-              metadata: metadata
+            const file = bucket.file(`photos/resized/${__newFile.fd}`);
+            const stream = file.createWriteStream({
+              metadata,
             });
             gm(__newFile)
-              .resize(300 + '>')
+              .resize(`${globalOpts.resizeDimension || 900}>`)
               .gravity('Center')
               .stream()
-              .pipe(stream)
-            stream.on('error', function (err) {
-              reject(err)
-              return;
-            })
-            stream.on('finish', function () {
+              .pipe(stream);
+            stream.on('error', (err) => {
+              reject(err);
+            });
+            stream.on('finish', () => {
               if (globalOpts.public) file.makePublic();
               resolve();
             });
-          }))
+          }));
 
           // Add thumb
           allUploads.push(new Promise((resolve, reject) => {
-            var file = bucket.file('photos/thumbs/' + __newFile.fd);
-            var stream = file.createWriteStream({
-              metadata: metadata
+            const file = bucket.file(`photos/thumbs/${__newFile.fd}`);
+            const stream = file.createWriteStream({
+              metadata,
             });
             gm(__newFile)
-              .resize(100, 100, "^")
+              .resize(100, 100, '^')
               .gravity('Center')
               .crop('100', '100')
               .stream()
-              .pipe(stream)
-            stream.on('error', function (err) {
-              reject(err)
-              return;
-            })
-            stream.on('finish', function () {
+              .pipe(stream);
+            stream.on('error', (err) => {
+              reject(err);
+            });
+            stream.on('finish', () => {
               if (globalOpts.public) file.makePublic();
               resolve();
             });
-          }))
+          }));
         }
         Promise.all(allUploads).then(() => done()).catch(err => receiver__.emit('error', err));
-      }
+      };
 
       return receiver__;
-    }
+    },
   };
-  return adapter
+  return adapter;
 };
